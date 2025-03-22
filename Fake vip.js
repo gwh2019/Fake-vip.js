1885880f6bd3a7f184aaf82a59dcbabd92d2490b
gwh2019 <47899009+gwh2019@users.noreply.github.com>
2025-03-22 10:55:45 +0800

    Update Fake vip.js

--- Fake vip.js
+++ Fake vip.js
@@ -1,1 +1,12 @@
+let body = $response.body;
+let obj = JSON.parse(body);
 
+// 修改 VIP 状态
+if (obj.data) {
+    obj.data.is_vip = true;    // 直接标记为 VIP
+    obj.data.vip_expire = "2099-12-31";  // 会员过期时间设定为 2099 年
+    obj.data.vip_level = 3;    // 设置最高等级 VIP
+}
+
+// 返回修改后的 JSON
+$done({ body: JSON.stringify(obj) });
